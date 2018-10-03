bd836d1334b9a0c5dfe8c8c29234d3f0ba673caa
sacullezzar <lucas.razzell@holidayextras.com>
2018-10-03 16:59:30 +0100

    end of step 11 (fo' real

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,7 +1,16 @@
 require "docking_station.rb"
 
 describe DockingStation do
-  it {is_expected.to respond_to :release_bike}
+  it { is_expected.to respond_to :release_bike }
+  it "docks something" do
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq bike
+  end
+  it "returns docked bikes" do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
+  end
 end
 
 describe DockingStation do
