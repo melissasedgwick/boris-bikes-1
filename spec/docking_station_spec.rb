6cb00e17b7cf3ed601f2e2023cdef787b2cc7559
sacullezzar <lucas.razzell@holidayextras.com>
2018-10-03 13:51:28 +0100

    end of step 10

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,12 @@
 require "docking_station.rb"
 
 describe DockingStation do
-  it { is_expected.to respond_to :release_bike}
+  it {is_expected.to respond_to :release_bike}
+end
+
+describe DockingStation do
+  it "releases a bike which working" do
+    bike = subject.release_bike
+    expect(bike).to be_working
+  end
 end
