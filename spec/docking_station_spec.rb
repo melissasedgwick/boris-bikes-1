17188933c27334e1d7de655f06c386e68760c285
sacullezzar <lucas.razzell@holidayextras.com>
2018-10-03 16:36:17 +0100

    stuck on step 12

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,16 +2,12 @@ require "docking_station.rb"
 
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
-  it { is_expected.to respond_to(:dock).with(1).argument }
-  it { is_expected.to respond_to(:bike) }
-end
-
-describe DockingStation do
   it "releases a bike which is working" do
     bike = subject.release_bike
     expect(bike).to be_working
   end
-
+  it { is_expected.to respond_to(:dock).with(1).argument }
+  it { is_expected.to respond_to(:bike) }
   it "docks something" do
     bike = Bike.new
     expect(subject.dock(bike)).to eq bike
@@ -21,5 +17,18 @@ describe DockingStation do
     bike = Bike.new
     subject.dock(bike)
     expect(subject.bike).to eq bike
+
+  end
+  describe "#release_bike" do
+    it "releases a bike" do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.release_bike).to eq bike
+  end
+end
+  describe '#release_bike' do
+    it 'raises an error when there are no bikes available' do
+      expect { subject.release_bike }.to raise_error 'No bikes available'
+    end
   end
 end
